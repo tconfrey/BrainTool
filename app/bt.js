ed5f0fa8b146e5a96c3ad739f35e47731747a114
tconfrey <tconfrey@gmail.com>
2020-07-27 14:24:57 -0400

    adding try/catch around gapi calls to catch spurious errors

--- app/bt.js
+++ app/bt.js
@@ -118,24 +118,31 @@ function appendPre(message) {
  */
 var fileid;
 function FindOrCreateBTFile() {
-    gapi.client.drive.files.list({
-        'pageSize': 1,
-        'fields': "files(id, name)",
-        'q': "name='BrainTool.org' and not trashed"
-    }).then(function(response) {
-        var files = response.result.files;
-        if (files && files.length > 0) {
-            var file = files[0];
-            fileid = file.id;
-            getBTFile();
-        } else {
-            console.log('BrainTool.org file not found.');
-            createStartingBT();
-        }
-    });
+    try {
+        gapi.client.drive.files.list({
+            'pageSize': 1,
+            'fields': "files(id, name)",
+            'q': "name='BrainTool.org' and not trashed"
+        }).then(function(response) {
+            var files = response.result.files;
+            if (files && files.length > 0) {
+                var file = files[0];
+                fileid = file.id;
+                getBTFile();
+            } else {
+                console.log('BrainTool.org file not found.');
+                createStartingBT();
+            }
+        });
+    }
+    catch (err) {   
+        alert("BT - error reading file list from GDrive. Check permissions and retry");
+        console.log("Error in writeBTFile: ", JSON.stringify(err));
+    }
 }
 
 function getBTFile() {
+    try {
     gapi.client.drive.files.get({
         fileId: fileid,
         alt: 'media'
@@ -146,6 +153,11 @@ function getBTFile() {
         function(error) {
             console.log("Error - Could not read BT file");
         });
+    }
+    catch(err) {
+        alert("BT - error reading BT file from GDrive. Check permissions and retry");
+        console.log("Error in writeBTFile: ", JSON.stringify(err));
+    }
 }
 
 
@@ -203,34 +215,34 @@ function writeBTFile() {
         'mimeType': 'text/plain' // mimeType at Google Drive
     };
     try {
-    var accessToken = gapi.auth.getToken().access_token; // Here gapi is used for retrieving the access token.
-    var form = new FormData();
-    console.log("writing BT file. accessToken = ", accessToken);
+        var accessToken = gapi.auth.getToken().access_token; // Here gapi is used for retrieving the access token.
+        var form = new FormData();
+        console.log("writing BT file. accessToken = ", accessToken);
 
-    form.append('metadata', new Blob([JSON.stringify(metadata)], { type: 'application/json' }));
-    form.append('file', new Blob([BTFileText], {type: 'text/plain'}));
+        form.append('metadata', new Blob([JSON.stringify(metadata)], { type: 'application/json' }));
+        form.append('file', new Blob([BTFileText], {type: 'text/plain'}));
 
-    fetch('https://www.googleapis.com/upload/drive/v3/files/' + encodeURIComponent(fileid) + '?uploadType=multipart',
-          {
-              method: 'PATCH', 
-              headers: new Headers({ 'Authorization': 'Bearer ' + accessToken }),
-              body: form
-          }).then((res) => {
-	          if (!res.ok) {
-		          alert("BT - error writing to GDrive, reuthenticating...");
-		          console.log("GAPI response:\n", res);
-                  gapi.auth.authorize(
-                      {client_id: CLIENT_ID, scope: SCOPES, immediate: true}
-                  ).then((res) => {
-                      if (res.status && res.status.signed_in) {
-                          writeBTFile();                        // try again
-                      }});
-		          return('GAPI error');
-	          }
-              return res.json();
-          }).then(function(val) {
-              console.log(val);
-          });
+        fetch('https://www.googleapis.com/upload/drive/v3/files/' + encodeURIComponent(fileid) + '?uploadType=multipart',
+              {
+                  method: 'PATCH', 
+                  headers: new Headers({ 'Authorization': 'Bearer ' + accessToken }),
+                  body: form
+              }).then((res) => {
+	              if (!res.ok) {
+		              alert("BT - error writing to GDrive, reuthenticating...");
+		              console.log("GAPI response:\n", res);
+                      gapi.auth.authorize(
+                          {client_id: CLIENT_ID, scope: SCOPES, immediate: true}
+                      ).then((res) => {
+                          if (res.status && res.status.signed_in) {
+                              writeBTFile();                        // try again
+                          }});
+		              return('GAPI error');
+	              }
+                  return res.json();
+              }).then(function(val) {
+                  console.log(val);
+              });
     }
     catch(err) {
         alert("BT - error writing to GDrive. Check permissions and retry");
