fdd65fd7905c762bedc637cb1cd38c2b02edc7cb
tconfrey <tconfrey@gmail.com>
2020-08-17 18:11:28 -0400

    swapping back to remotely hosted

--- extension/popup.js
+++ extension/popup.js
@@ -65,8 +65,8 @@ function windowOpen() {
 
     // Create window, remember it and highlight it
     var wargs = {
-        'url' : "http://localhost:8000/app", // "https://tconfrey.github.io/BrainTool/app", 
-//        'url' : "https://BrainTool.org/app", 
+//        'url' : "http://localhost:8000/app", // "https://tconfrey.github.io/BrainTool/app", 
+        'url' : "https://BrainTool.org/app", 
         'type' : "panel",
         'top' : 10, 'left' : 10,
         'width' : 500, 'height' : 1100
