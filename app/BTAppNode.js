ebaa2a274b8815f3d856d2819cd39b6d966213cb
tconfrey <tconfrey@gmail.com>
2020-08-27 15:54:54 -0400

    update to handle id: type org links

--- app/BTAppNode.js
+++ app/BTAppNode.js
@@ -133,7 +133,7 @@ class BTAppNode extends BTNode {
         let outputStr = txt;
         while (hits = reg.exec(outputStr)) {
             const h2 = (hits[2]=="undefined") ? hits[1] : hits[2];
-            if (hits[1].indexOf('file:') == 0)
+            if ((hits[1].indexOf('file:') == 0) || (hits[1].indexOf('id:') == 0))       // internal org links get highlighted, but not as hrefs
                 outputStr = outputStr.substring(0, hits.index) + "<span class='file-link'>" + h2 + "</span>" + outputStr.substring(hits.index + hits[0].length);
             else                
                 outputStr = outputStr.substring(0, hits.index) + "<a href='" + hits[1] + "' class='btlink'>" + h2 + "</a>" + outputStr.substring(hits.index + hits[0].length);
