274e09dfb9a990df3d2a428c8f07089e71d2faad
tconfrey <tconfrey@gmail.com>
2021-02-15 13:10:12 -0500

    added utilities folder w a TabsOutliner converter function

---
+++ utilities/converters.js
@@ -0,0 +1,40 @@
+/***
+ *
+ * Conversion utilities. Currently just from TabsOutliner json format
+ * 
+ ***/
+
+// 0) Export from Tabs Outliner to a local file
+// 1) Paste the code below as a Code Snippet in Chrome Dev Tools
+// 2) Copy the Tabs Outliner exported file contents between the `backticks` in the TabsOutlinerExport variable
+// 3) Run the snippet
+// 4) Copy the output into your BrainTool.org file and save it
+// 5) Refresh, or rerun, BrainTool
+
+const TabsOutlinerExport = ``;
+function tabsToBT(tabsStr) {
+    // take a TO export str and output a BT orgmode equivalent
+    const tabsJson = JSON.parse(tabsStr);
+    const lastIndex = tabsJson.length - 1;
+    let node, title, BTText = "";
+    tabsJson.forEach((elt, ind) => {
+        if (!ind || ind == lastIndex) return;
+        const info = elt[1];
+        const nesting = elt[2];
+        if (info.type && (info.type == 'win' || info.type == 'savedwin')) {
+            node = '*'.repeat(nesting.length);
+            title = (info.marks && info.marks.customTitle) ? info.marks.customTitle : 'Window';
+            node += ` ${title}\n`;
+            BTText += node;
+        }
+        if (info.data && info.data.url) {
+            node = '*'.repeat(nesting.length);
+            title = (info.marks && info.marks.customTitle) ? info.marks.customTitle : info.data.title;
+            node += ` [[${info.data.url}][${title}]]\n`;
+            BTText += node;
+        }
+    });
+    console.log("Copy this text to your BrainTool.org file:\n");
+    console.log(BTText);
+}
+tabsToBT(TabsOutlinerExport);
