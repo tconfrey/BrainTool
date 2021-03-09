b7ec1bcaa1e6f53c8db2eeb202ad4064ab439415
tconfrey <tconfrey@gmail.com>
2021-03-09 14:03:51 -0500

    iterated on Tabs Outliner import. Created input page

--- utilities/converters.js
+++ utilities/converters.js
@@ -1,36 +1,42 @@
 /***
  *
  * Conversion utilities. Currently just from TabsOutliner json format
+ * Use in conjunction with associated converters.md file
  * 
  ***/
 
-// 0) Export from Tabs Outliner to a local file
-// 1) Paste the code below as a Code Snippet in Chrome Dev Tools
-// 2) Copy the Tabs Outliner exported file contents between the `backticks` in the TabsOutlinerExport variable
-// 3) Run the snippet
-// 4) Copy the output into your BrainTool.org file and save it (either replacing everything or appending the new nodes)
-// 5) Refresh, or rerun, BrainTool
 
-const TabsOutlinerExport = ``;
+function getDateString() {
+    // return minimal date representation to append to bookmark tag
+    const d = new Date();
+    const mins = d.getMinutes() < 10 ? "0"+d.getMinutes() : d.getMinutes();
+    return (`${d.getMonth()+1}/${d.getDate()}/${d.getYear()-100} ${d.getHours()}:${mins}`);
+}
+
+
 function tabsToBT(tabsStr) {
     // take a TO export str and output a BT orgmode equivalent
+    
     const tabsJson = JSON.parse(tabsStr);
     const lastIndex = tabsJson.length - 1;
-    let node, title, BTText = "";
+    let node, title, numwin = 1;
+    let BTText = "* TabsOutliner Import - " + getDateString() + "\n";
     tabsJson.forEach((elt, ind) => {
-        if (!ind || ind == lastIndex) return;   // ignore first and last elements, TO seems to use them for some special purpose 
+        // ignore first and last elements, TO seems to use them for some special purpose 
+        if (!ind || ind == lastIndex) return;   
         const info = elt[1];
         const nesting = elt[2];
         // Handle window/container type elements
         if (info.type && (info.type == 'win' || info.type == 'savedwin')) {
-            node = '*'.repeat(nesting.length);
-            title = (info.marks && info.marks.customTitle) ? info.marks.customTitle : 'Window';
+            node = '*'.repeat(nesting.length + 1);
+            title = (info.marks && info.marks.customTitle) ?
+                info.marks.customTitle : 'Window'+numwin++;
             node += ` ${title}\n`;
             BTText += node;
         }
         // Handle tab/link type elements
         if (info.data && info.data.url) {
-            node = '*'.repeat(nesting.length);
+            node = '*'.repeat(nesting.length+1);
             title = (info.marks && info.marks.customTitle) ? info.marks.customTitle : info.data.title;
             node += ` [[${info.data.url}][${title}]]\n`;
             BTText += node;
@@ -38,5 +44,6 @@ function tabsToBT(tabsStr) {
     });
     console.log("Copy this text to your BrainTool.org file:\n");
     console.log(BTText);
+    return BTText;
 }
-tabsToBT(TabsOutlinerExport);
+
---
+++ utilities/converters.md
@@ -0,0 +1,28 @@
+---
+title: BrainTool Converter Utilities
+description: Convery other formats to BrainTool org-mode markup
+---
+
+# Tabs Outliner Import
+This tool imports from Tabs Outliner, converts to BrainTool org-mode format and imports into your BrainTool.org file.
+
+First export your data from Tabs Outliner and save the file somewhere locally. Then select it using the file chooser below. Copy the generated org-mode version and paste it into the bottom of your BrainTool.org file.
+
+<input type="file" name="inputfile" id="inputfile"> 
+<br> 
+
+<textarea id="output" style="border:solid; border-width: 1px; white-space: pre-wrap; width:100%;" rows="30"></textarea> 
+
+<script src="converters.js"></script>
+<script type="text/javascript"> 
+document.getElementById('inputfile').addEventListener('change', function() { 
+
+var fr=new FileReader(); 
+fr.onload=function(){ 
+var bt = tabsToBT(fr.result);
+document.getElementById('output').textContent=bt; 
+} 
+
+fr.readAsText(this.files[0]); 
+}) 
+</script> 
