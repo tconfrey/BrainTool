ef5b726f506c5f3f06271410e46980d64b09f24b
tconfrey <tconfrey@gmail.com>
2021-04-09 16:05:29 -0400

    mkver script missed /utilities

--- mkver.sh
+++ mkver.sh
@@ -4,9 +4,11 @@ echo creating versions/$1
 mkdir versions/$1
 mkdir versions/$1/app
 mkdir versions/$1/extension
+mkdir versions/$1/utilities
 mkdir versions/$1/app/resources
 mkdir versions/$1/extension/images
 cp app/* versions/$1/app
 cp extension/* versions/$1/extension/
+cp utilities/* versions/$1/utilities/
 cp app/resources/* versions/$1/app/resources
 cp extension/images/* versions/$1/extension/images
