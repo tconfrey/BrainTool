a90e16e7023783fc652e3b524ce6592f3bb4fd86
Tommy Jollyboat <11634808+ComedyTomedy@users.noreply.github.com>
2022-05-26 15:33:48 +0100

    Swap cp for rsync

--- mkver.sh
+++ mkver.sh
@@ -4,16 +4,13 @@
 ## Example: mkver.sh 1.0
 
 VERSION="$1"
-NEW="versions/$VERSION/"
-mkdir -p "$NEW"
+OUT="versions/$VERSION/"
+mkdir -p "$OUT"
 
-# NOTE cp on Mac/BSD and Linux behaves differently
-# DON'T add a trailing slash to the source directories
-# or Mac/BSD will copy contents instead of the whole directory
-cp -aR app extension utilities "$NEW" &&
-    echo "Created $NEW"
+# copy whole folders
+rsync -a app extension utilities "$OUT"
 
 # Update manifest files to point to the right version
-sed -i '/"version":/s/:.*/: "'$VERSION'",/' $NEW/extension/manifest*.json
+sed -i '/"version":/s/:.*/: "'$VERSION'",/' $OUT/extension/manifest*.json
 
 # Do popup files need updating too?
