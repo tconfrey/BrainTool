bb29df2b410cd595962938f04b4622cc4a09fba1
tconfrey <tconfrey@gmail.com>
2024-06-02 15:55:05 -0400

    stupid mistake. left RC candidate pointing ot local server

--- versions/Release-Candidate/extension/popup.js
+++ versions/Release-Candidate/extension/popup.js
@@ -129,7 +129,8 @@ async function windowOpen(home = 'PANEL', location) {
     // Create window, remember it and highlight it
     const version = chrome.runtime.getManifest().version;
    // const url = "https://BrainTool.org/app/";
-    const url = "http://localhost:8000/app/"; // versions/"+version+"/app/";
+    const url = "https://BrainTool.org/versions/Release-Candidate/app/";
+    //const url = "http://localhost:8000/app/"; // versions/"+version+"/app/";
    // const url = "https://BrainTool.org/versions/"+version+'/app/';
     console.log('loading from ', url);
 
